4b0e8ec3999be5db3f75c0369a697ab4383acab7
MuseMath <mus3math@ymail.com>
2016-02-18 20:44:15 +0000

    added else if statements to set default rows and guesses if left 0

--- Game/src/Difficulty.java
+++ Game/src/Difficulty.java
@@ -1,35 +1,48 @@
 
 public class Difficulty {
-	
-	Difficulty(){}
-	
-	// Sets max and min parameters for the class
-	final private int  	MIN_ROWS=3, 	MAX_ROWS=7,
-						MIN_GUESSES=3, 	MAX_GUESSES=7;
-	int NumRows, NumGuesses;
-	
-	
-	public int getRows(){
-		return 0;
-	}
-	
-	public void setRows(int rows){
-		
-	}
-	
-	public int getGuesses(){
-		return 0;
-	}
-	
-	public void setGuesse(int guesses){
-		
-	}
-	
-	// Preset dificulties, it will have a default value for 'NumRows' & 'NumGuesses'
-	public void setEasy(){}
-	
-	public void setMedium(){}
 
-	public void setHard(){}
-	
+	// Sets max and min parameters for the class
+	final private int  	MIN_ROWS=3, 	MAX_ROWS=7,	MIN_GUESSES=3, 	MAX_GUESSES=7;
+	int easyRows, easyGuesses, mediumrows, mediumguesses;
+
+
+	public void setEsayRows(int erows){
+
+		if(erows == 0){easyRows = MIN_ROWS;}
+		else{easyRows = erows;}
+	}
+
+	public int getEasyRows(){
+		return 	easyRows;
+	}
+
+	public void setEasyGuesses(int eguesses){
+
+		if(eguesses == 0){easyGuesses = MIN_GUESSES;}
+		else{easyGuesses = eguesses;}
+	}
+
+	public int getEasyGuesses(){
+		return easyGuesses;
+	}
+
+	public void setMediumRows(int mrows){
+
+		if(mrows == 0){mediumrows = MAX_ROWS;}
+		else{mediumrows = mrows;}
+	}
+
+	public int getMediumRows(){
+		return 	mediumrows;
+	}
+
+	public void setMediumGuesses(int mguesses){
+
+		if(mguesses == 0){mediumguesses = MIN_GUESSES;}
+		else{mediumguesses = mguesses;}
+	}
+
+	public int getMediumuesses(){
+		return mediumguesses;
+	}
 }
