be4e482c90e6bc903d08bc3be24a1e5c13b1b425
Gian Brazzini <gianbrazzini@hotmail.com>
2016-04-14 16:05:13 -0500

    Updated to randomize selection of words

--- Game/src/games/GetWordList.java
+++ Game/src/games/GetWordList.java
@@ -1,6 +1,8 @@
 package games;
 
 import java.util.LinkedList;
+import java.util.Random;
+import java.util.Scanner;
 import java.net.*;
 import java.io.*;
 
@@ -9,18 +11,26 @@ public class GetWordList
 
 	public LinkedList<String> WordList()
 	{
+		
+		Random skip = new Random(1);
+		
 		LinkedList<String> list = new LinkedList<String>();
 		int counter = 0;
 		int maxWords = 1000;
 		try
 		{
-			URL wordlist = new URL( "https://raw.githubusercontent.com/dwyl/english-words/master/words2.txt" );
-			BufferedReader in = new BufferedReader( new InputStreamReader( wordlist.openStream() ) );
+			File wordlist = new File( "words_english.txt" );
+			Scanner in = new Scanner( wordlist );
 
 			String inputLine;
 
-			while ( (inputLine = in.readLine()) != null)
+			while ( (inputLine = in.nextLine()) != null)
 			{
+				
+				for(int i=Math.abs(skip.nextInt()%100);i>0;i--){
+					inputLine = in.nextLine();
+				}
+				
 				list.add( inputLine );
 				counter++;
 				if ( counter == maxWords ) break;
